d82773e6755e6b46fcc0a3429711312dc63ea46c
Hubert <hubert.rola@microsoft.wsei.edu.pl>
2026-04-17 15:04:11 +0200

    Lab06: Ukrycie kluczy Firebase w .env i aktualizacja .gitignore

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,22 +1,15 @@
-// src/lib/firebase.ts
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDXGh45tZblm9evrz1keyu06-HPj_njwVA",
-  authDomain: "managemeapp-6380a.firebaseapp.com",
-  projectId: "managemeapp-6380a",
-  storageBucket: "managemeapp-6380a.firebasestorage.app",
-  messagingSenderId: "199497521959",
-  appId: "1:199497521959:web:dec9985dabc812a7cc2702",
-  measurementId: "G-NJ6G1X143L"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
-// Inicjalizacja (sprawdza czy apka już nie istnieje, żeby uniknąć błędów przy odświeżaniu)
 const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
-
-// EKSPORTUJEMY auth i provider, aby App.tsx mógł ich użyć
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
-
-export default app;
